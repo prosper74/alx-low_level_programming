026bba545b0818a43f2d2d0fa2948ba40e875118
Prosper Atu <atu.prosper@gmail.com>
2023-10-05 10:10:57 -0700

    fixed betty error for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main - entry point
  *
- * return: always 0 (which is Success)
+ * Return: always 0 (which is success)
  */
 
-int main (void) {
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
