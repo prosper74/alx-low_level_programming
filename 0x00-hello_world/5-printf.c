97b1b4c99a502160f51299c7edb8139ded524373
Prosper Atu <atu.prosper@gmail.com>
2023-10-05 09:58:39 -0700

    task 5 error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * return: always 0 (which is success)
- * /
+ * Return: Always 0 (Success)
+ */
 
 int main (void) {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
