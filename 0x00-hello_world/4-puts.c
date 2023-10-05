2370d44de20d41d928f352bf46c3b50e2d604fc2
Prosper Atu <atu.prosper@gmail.com>
2023-10-05 09:44:33 -0700

    task 4 - hello, puts)

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: always 0 (which is success)
+ * /
+
+int main (void) {
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
