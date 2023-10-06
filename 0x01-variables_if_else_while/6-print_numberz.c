e9bf5672ea086f53c3ccfd84b495086ec4987697
Prosper Atu <atu.prosper@gmail.com>
2023-10-06 07:04:35 -0700

    task 6 - base 10 digits without using char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * A program that prints all single digit numbers of base 10 starting from 0
+ *
+ * Do not use char
+ *
+ * Return: returns 0 (for success)
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
