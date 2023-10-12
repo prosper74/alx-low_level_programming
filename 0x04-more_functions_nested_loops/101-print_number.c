6025029d355ff688f3973d91539eb2ec02961316
Prosper Atu <atu.prosper@gmail.com>
2023-10-12 12:22:04 -0700

    task 12 - print integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ * @num: number to be printed
+ */
+
+void print_number(int num)
+{
+	unsigned int tens, digit, positiveNumber = num;
+	double intChecker = 1;
+
+	if (num == 0)
+		_putchar('0');
+	else
+	{
+		if (num < 0)
+		{
+			positive = num * -1;
+			_putchar('-');
+		}
+
+		while (intChecker <= positiveNumber)
+			intChecker *= 10;
+		tens = intChecker / 10;
+
+		while (tens >= 1)
+		{
+			digit = positiveNumber / tens;
+			_putchar(digit + '0');
+			positiveNumber = (positiveNumber - (tens * digit));
+			tens /= 10;
+		}
+	}
+}
