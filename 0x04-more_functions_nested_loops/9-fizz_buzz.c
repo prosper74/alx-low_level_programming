8d22b45e29136b716235286e9099332fafbff81a
Prosper Atu <atu.prosper@gmail.com>
2023-10-12 11:14:40 -0700

    task 9 - fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <main.h>
+
+/**
+ * main - fizz buzz program,
+ * multiples of 3 print Fizz,
+ * multiples of 5 print Buzz
+ * and multiple of both 3 and 5 print FizzBuzz
+ * Return: returns 0 on success
+ */
+
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if (count % 3 == 0 && count % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (count % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (count % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", count);
+		}
+
+		if (count < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
