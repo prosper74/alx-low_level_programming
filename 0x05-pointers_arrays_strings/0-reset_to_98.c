e60b639cbe8ffa87095559343551f719e5012648
Prosper Atu <atu.prosper@gmail.com>
2023-10-16 10:02:32 -0700

    Pointers and arrays in C, task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ *
+ * @num: num is the pointer integer
+ */
+void reset_to_98(int *num)
+{
+	*num = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointers, Array and string manipulations in C
