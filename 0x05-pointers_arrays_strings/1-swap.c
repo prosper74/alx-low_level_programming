e31735fb73b51f1b99c1719218c1d365187837b1
Prosper Atu <atu.prosper@gmail.com>
2023-10-16 10:10:18 -0700

    task 1 - swap integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers
+ * 
+ * @num1: first integer
+ * @num2: second integer
+ */
+void swap_int(int *num1, int *num2)
+{
+	int num3 = *num2, num4 = *num1;
+	*num1 = num3;
+	*num2 = num4;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,4 +3,6 @@
 
 void reset_to_98(int *num);
 
+void swap_int(int *num1, int *num2);
+
 #endif
