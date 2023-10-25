5cb25d46bd80a576e9b56ece2ce46ede6365d9fa
Prosper Atu <atu.prosper@gmail.com>
2023-10-25 13:46:29 -0700

    task 3 error

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,13 +11,11 @@
 int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	} else if (n <= 1)
-	{
+	else if (n == 0)
 		return (1);
-	} else
-	{
+	else if (n > 1)
 		return (n * factorial(n - 1));
-	}
+	else
+		return (n);
 }
