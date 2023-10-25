d2583beec62209cf0c6293b87bb59ddff910b783
Prosper Atu <atu.prosper@gmail.com>
2023-10-25 13:10:26 -0700

    task 0 output error

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
-* _puts_recursion - a function that prints a string,
-* followed by a new line.
-* @str: The given string arguement
-*/
+ * _puts_recursion - a function that prints a string,
+ * followed by a new line.
+ * @str: The given string arguement
+ */
 
 void _puts_recursion(char *str)
 {
-if (*str == '\0')
-{
-	_putchar('\n');
-}
-
-_putchar(*str);
-_puts_recursion(str + 1);
+	if (*str != '\0')
+	{
+		_putchar(*str);
+		_puts_recursion(str + 1);
+	}
+	else if (*str == '\0')
+	{
+		_putchar('\n');
+	}
 }
