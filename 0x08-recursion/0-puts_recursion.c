fcbe9181d63fd7902c0fbdc2ce306efbe6795bbe
Prosper Atu <atu.prosper@gmail.com>
2023-10-25 13:05:31 -0700

    Recursive functions task 0 - puts character

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts_recursion - a function that prints a string,
+* followed by a new line.
+* @str: The given string arguement
+*/
+
+void _puts_recursion(char *str)
+{
+if (*str == '\0')
+{
+	_putchar('\n');
+}
+
+_putchar(*str);
+_puts_recursion(str + 1);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char character);
+
+void _puts_recursion(char *s);
+
+#endif
