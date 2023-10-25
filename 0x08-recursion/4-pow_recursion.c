76bcf35018fc1d096af4c718710642963b9ba617
Prosper Atu <atu.prosper@gmail.com>
2023-10-25 14:02:08 -0700

    task 4 - power of a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _pow_recursion - a recursive function
+* that returns the value of x raised to the power of y.
+* @x: The number to raise
+* @y: The power
+*
+* Return: Value of x to the power of y
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+	return (-1);
+}
+else if (y == 0)
+{
+	return (1);
+}
+else if (y > 0)
+{
+	return (x * _pow_recursion(x, y - 1));
+}
+
+return (x * _pow_recursion(x, y - 1));
+}
