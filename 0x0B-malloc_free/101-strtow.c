13e579d1c6ff617f72f49a45192e03bbccffebd1
Prosper Atu <atu.prosper@gmail.com>
2023-10-31 04:52:24 -0700

    output error on task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,88 +1,77 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * strtow_helper - helps function for string to word function
- * @word: wordcount
- * @length: length
- * @str: string to go through
- * @s: array you are assigning
- * Return: char value
+ * word_count_helper - Count number of words
+ * @str: String to check
+ *
+ * Return: Number of words;
  */
 
-char **strtow_helper(int word, int length, char *str, char **s)
+int word_count_helper(char *str)
 {
-	int count1, count2 = 0, count3;
+	int flag = 0, count, wordCount = 0;
 
-	for (count1 = 0; count1 < word; count1++)
+	for (count = 0; str[count] != '\0'; count++)
 	{
-		count3 = 0;
-		for (; count2 < length; count2++)
+		if (str[count] == ' ')
+			flag = 0;
+		else if (flag == 0)
 		{
-			if (str[0] != ' ' || str[count2] != ' ')
-			{
-				s[count1][count3] = str[count2];
-				count3++;
-			}
-			if (count2 != 0 && str[count2] == ' ' && str[count2 - 1] != ' ')
-			{
-				count2++;
-				break;
-			}
+			flag = 1;
+			wordCount++;
 		}
-		s[count1][count3 + 1] = '\0';
 	}
-	s[word + 1] = NULL;
-	return (s);
+
+	return (wordCount);
 }
 
 /**
  * strtow - a function that splits a string into words.
- * @str: string to check
- * Return: return char value
+ * @str: String to split
+ *
+ * Return: NULL if str == NULL or str == ""
  */
 
 char **strtow(char *str)
 {
-	int length, count1, count2, count3, size, word;
-	char **s;
+	char **matrix, *tmp;
+	int count1, count2 = 0, len = 0, words, count = 0, start, end;
 
-	if (str == NULL)
+	while (*(str + len))
+		len++;
+	words = word_count_helper(str);
+	if (words == 0)
 		return (NULL);
-	length = 0;
-	word = 0;
-	while (str[length] != '\0')
-	{
-		if (str[0] != ' ')
-		word++;
-		if (str[length] != ' ' && str[length - 1] == ' ' && length != 0)
-			word++;
-		length++;
-	}
-	s = (char **)malloc(sizeof(char *) * word + 1);
-	if (s == NULL)
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
 		return (NULL);
-	count2 = 0;
-	for (count1 = 0; count1 < word; count1++)
+
+	for (count1 = 0; count1 <= len; count1++)
 	{
-		size = 0;
-		for (; count2 < length; count2++)
+		if (str[count1] == ' ' || str[count1] == '\0')
 		{
-			if (str[0] != ' ' || str[count2] != ' ')
-				size++;
-			if (str[count2] == ' ' && size > 0)
-				break;
+			if (count)
+			{
+				end = count1;
+				tmp = (char *) malloc(sizeof(char) * (count + 1));
+				if (tmp == NULL)
+					return (NULL);
+
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[count2] = tmp - count;
+				count2++;
+				count = 0;
+			}
 		}
-		printf("%d\n", size);
-		s[count1] = (char *)malloc(sizeof(char) * size + 1);
-		if (s[count1] == NULL)
-		{
-			for (count3 = count1 - 1; count3 >= 0; count3--)
-				free(s[count3]);
-			free(s);
-		}
+		else if (count++ == 0)
+			start = count1;
 	}
-	s = strtow_helper(word, length, str, s);
-	return (s);
+
+	matrix[count2] = NULL;
+
+	return (matrix);
 }
