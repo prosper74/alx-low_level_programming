3899be3f9557b4dfa28177e13c286dae3d5478da
Prosper Atu <atu.prosper@gmail.com>
2023-11-02 12:25:38 -0700

    more functions on malloc and free, task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ * @b: Size of memory to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *reservedMemory;
+
+	reservedMemory = malloc(b);
+	if (reservedMemory == NULL)
+		exit(98);
+	return (reservedMemory);
+}
