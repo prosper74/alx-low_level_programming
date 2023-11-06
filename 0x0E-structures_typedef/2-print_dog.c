592213f7b24cb8764a9e6a13874fe70dabaf4dc5
Prosper Atu <atu.prosper@gmail.com>
2023-11-06 08:31:01 -0800

    task 2 - fixed output error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 	(d->name == NULL) ? printf("Name: (nil)\n")
 		: printf("Name: %s\n", d->name);
 
-	printf("Age: %.1f\n", d->age);
+	printf("Age: %f\n", d->age);
 
 	(d->owner == NULL) ? printf("Owner: (nil)\n")
 		: printf("Owner: %s\n", d->owner);
