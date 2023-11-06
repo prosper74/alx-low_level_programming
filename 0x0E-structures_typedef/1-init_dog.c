3f4c732ce8072622baa0e1d3dd702efa575031a5
Prosper Atu <atu.prosper@gmail.com>
2023-11-06 08:01:36 -0800

    task 1 - initialize dog struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - a function that initialize a variable of type struct dog
+ * @d: a variable of type struct dog
+ * @name: variable that holds the dogs name
+ * @age: variable that holds the dog age
+ * @owner: variable that holds the dog owner name
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	if (d == NULL)
+		return (NULL);
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
