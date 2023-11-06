cc1796e3ac092a976be3e0e12d4d0eaf84ab92e8
Prosper Atu <atu.prosper@gmail.com>
2023-11-06 10:21:16 -0800

    task 5 - free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - a function that frees dogs.
+ * @d: instance to be freed
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
