1a1595b0a437a75e0e8e062c0d915ee6af0f0645
Prosper Atu <atu.prosper@gmail.com>
2023-11-06 08:22:33 -0800

    task 2 - print dog struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - a function that prints a struct dog
+ * If d is NULL print nothing.
+ * @d: a variable of type struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+
+	if (d == NULL)
+		return;
+
+	(d->name == NULL) ? printf("Name: (nil)\n")
+		: printf("Name: %s\n", d->name);
+
+	printf("Age: %.1f\n", d->age);
+
+	(d->owner == NULL) ? printf("Owner: (nil)\n")
+		: printf("Owner: %s\n", d->owner);
+
+}
