56c833424f1e66118748e286ecb1a261b4c64a94
Prosper Atu <atu.prosper@gmail.com>
2023-11-08 05:43:29 -0800

    task 0 - print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name.
+ * @name: parameter that function pointer needs
+ * @f: function pointer
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+	{
+		f(name);
+	}
+}
