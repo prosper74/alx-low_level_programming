be976cce2f6c82f69efa50f30d570e2f9dcf6f90
Prosper Atu <atu.prosper@gmail.com>
2023-11-16 12:58:19 -0800

    task 4 and 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_before_main - Print string before the main program runs.
+ */
+void print_before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
+
