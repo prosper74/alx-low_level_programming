966448e00382cd9ba34a29200e630554d8117b47
Prosper Atu <atu.prosper@gmail.com>
2023-11-16 08:04:09 -0800

    task 4 - free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* free_list - Frees a list_t list.
+* @head: A pointer to the list_t list.
+*
+* Return: void.
+*/
+void free_list(list_t *head)
+{
+list_t *current;
+
+while (head != NULL)
+{
+	current = head;
+	head = head->next;
+	free(current->str);
+	free(current);
+}
+}
+
