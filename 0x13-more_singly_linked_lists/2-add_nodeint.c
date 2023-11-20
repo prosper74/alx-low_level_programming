9b4bdfc6024ac065e84d1095219f8f365d07ee56
Prosper Atu <atu.prosper@gmail.com>
2023-11-20 05:14:12 -0800

    task 2 - add new node at the beginning

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* add_nodeint - Prints all the elements of a list_t list.
+* @head: A pointer to the listint_t list.
+* @n: The data to add to the list
+*
+* Return: The new node or null on failure
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
+if (new_node == NULL)
+{
+	return (NULL);
+}
+new_node->n = n;
+new_node->next = *head;
+*head = new_node;
+return (new_node);
+}
+
