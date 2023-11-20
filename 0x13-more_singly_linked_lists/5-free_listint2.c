134ec9fb19c50f4b73eacebce1ace650365f6998
Prosper Atu <atu.prosper@gmail.com>
2023-11-20 10:07:04 -0800

    task 5 - fixing output error

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,23 +1,26 @@
 #include "lists.h"
 
 /**
-* free_listint2 - Function that frees the list and make the head = null
-* @head: A pointer to the listint_t list head.
-*/
+ * free_listint2 - Function that frees the list and make the head = null
+ * @head: A pointer to the listint_t list head.
+ */
 
 void free_listint2(listint_t **head)
 {
-listint_t *current_node = *head;
-listint_t *next_node;
+	listint_t *current_node = *head;
+	listint_t *next_node;
 
-while (current_node != NULL)
-{
-	next_node = current_node->next;
+	if (head != NULL)
+	{
+		while (current_node != NULL)
+		{
+			next_node = current_node->next;
 
-	free(current_node);
-	current_node = next_node;
-}
+			free(current_node);
+			current_node = next_node;
+		}
 
-*head = NULL;
+		*head = NULL;
+	}
 }
 
