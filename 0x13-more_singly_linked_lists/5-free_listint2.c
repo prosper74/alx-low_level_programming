46ba76c5e4fbd0fbd45ae20473ffe8a58af8322f
Prosper Atu <atu.prosper@gmail.com>
2023-11-20 06:07:38 -0800

    task 5 - free list and make head null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* free_listint2 - Function that frees the list and make the head = null
+* @head: A pointer to the listint_t list head.
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *current_node = *head;
+listint_t *next_node;
+
+while (current_node != NULL)
+{
+	next_node = current_node->next;
+
+	free(current_node);
+	current_node = next_node;
+}
+
+*head = NULL;
+}
+
