31e9a8574b6f6b6d1f5fe71a2fe561e8ddd65542
Prosper Atu <atu.prosper@gmail.com>
2023-11-20 10:10:41 -0800

    task 5 - fixing output error..

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,19 +7,18 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current_node = *head;
-	listint_t *next_node;
+	listint_t *free_node;
+	listint_t *current_node;
 
 	if (head != NULL)
 	{
+		current_node = *head;
 		while (current_node != NULL)
 		{
-			next_node = current_node->next;
-
-			free(current_node);
-			current_node = next_node;
+			free_node = current_node;
+			current_node = current_node->next;
+			free(free_node);
 		}
-
 		*head = NULL;
 	}
 }
