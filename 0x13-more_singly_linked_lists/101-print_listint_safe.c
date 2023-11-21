95b3bd27562a0f7ad70b02764c7f3f0ad14d9adb
Prosper Atu <atu.prosper@gmail.com>
2023-11-21 10:47:56 -0800

    task 12 - fixing output error...

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,39 +1,40 @@
 #include "lists.h"
 
 /**
-* print_listint_safe - Function that prints a listint_t linked list.
+* print_listint_safe - Prints a listint_t linked list safely.
 * @head: A pointer to the listint_t list head.
 *
-* Return: The number of nodes in the list
+* Return: The number of nodes in the list.
 */
-
 size_t print_listint_safe(const listint_t *head)
 {
-size_t count = 0, i;
-const listint_t **list = NULL;
+const listint_t *current_node = NULL;
+const listint_t *check_node = NULL;
+size_t counter = 0, i;
 
-while (head != NULL)
+current_node = head;
+
+while (current_node)
 {
-	for (i = 0; i < count; i++)
+	printf("[%p] %d\n", (void *)current_node, current_node->n);
+	counter++;
+	current_node = current_node->next;
+	check_node = head;
+
+	for (i = 0; i < counter; i++)
 	{
-	if (head == list[i])
+	if (current_node == check_node)
 	{
-		printf("-> [%p] %d\n", (void *)head, head->n);
-		return (count);
+		printf("-> [%p] %d\n", (void *)current_node, current_node->n);
+		return (counter);
 	}
+	check_node = check_node->next;
 	}
 
-	printf("[%p] %d\n", (void *)head, head->n);
-	count++;
-	list = realloc(list, count * sizeof(listint_t *));
-
-	if (list == NULL)
+	if (!head)
 	exit(98);
-
-	list[count - 1] = head;
-	head = head->next;
 }
-free(list);
-return (count);
+
+return (counter);
 }
 
