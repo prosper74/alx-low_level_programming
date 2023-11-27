0c705814aba6c719af43999aecc7eb23c217614b
Prosper Atu <atu.prosper@gmail.com>
2023-11-27 14:51:21 -0800

    task 3 - added buffer to main file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+
+#define BUFSIZE 1204
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
