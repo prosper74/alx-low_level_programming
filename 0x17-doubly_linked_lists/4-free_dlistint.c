5cb8fe9e41b8e362af7f1b04926a8fb8261f3e15
Prosper Atu <atu.prosper@gmail.com>
2024-01-11 06:29:53 -0800

    task 4 - free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* free_dlistint - Frees a dlistint_t list
+* @head: Pointer to the head of the doubly linked list
+*/
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *current, *next;
+
+current = head;
+while (current != NULL)
+{
+	next = current->next;
+	free(current);
+	current = next;
+}
+}
+
