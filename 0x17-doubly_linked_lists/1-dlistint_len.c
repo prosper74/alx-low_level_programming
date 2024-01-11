b497be0ce6b6d1a879370f611ccb580f19a43c24
Prosper Atu <atu.prosper@gmail.com>
2024-01-11 00:20:59 -0800

    task 1 - number of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Returns the number of elements in a dlistint_t list
+ * @h: Pointer to the head of the doubly linked list
+ * Return: The number of nodes in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t node_count = 0;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		node_count++;
+	}
+
+	return (node_count);
+}
+
