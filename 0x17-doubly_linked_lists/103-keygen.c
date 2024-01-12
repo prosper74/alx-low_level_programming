023e3a7b12bb1587888369bb827ae1f5eb155955
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-01-12 11:09:44 +0100

    Update 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -2,119 +2,123 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define MAX_KEY_LEN 7
+#define RAND_MOD 63
+
+int get_max_char(char *str, int len);
+int multiply_chars(char *str, int len);
+int generate_rand_char(char *str);
+
 /**
- * f4 - finds the biggest number
- *
- * @usrn: username
+ * get_max_char - finds the biggest number
+ * @str: username
  * @len: length of username
+ *
  * Return: the biggest number
  */
-int f4(char *usrn, int len)
+int get_max_char(char *str, int len)
 {
-	int ch;
-	int vch;
-	unsigned int rand_num;
+	int max_char, current_char;
+	unsigned int random_number;
 
-	ch = *usrn;
-	vch = 0;
+	current_char = *str;
+	max_char = 0;
 
-	while (vch < len)
+	while (max_char < len)
 	{
-		if (ch < usrn[vch])
-			ch = usrn[vch];
-		vch += 1;
+		if (current_char < str[max_char])
+			current_char = str[max_char];
+		max_char += 1;
 	}
 
-	srand(ch ^ 14);
-	rand_num = rand();
-
-	return (rand_num & 63);
+	srand(current_char ^ 14);
+	random_number = rand();
+	return (random_number & RAND_MOD);
 }
 
 /**
- * f5 - multiplies each char of username
- *
- * @usrn: username
+ * multiply_chars - multiplies each char of username
+ * @str: username
  * @len: length of username
+ *
  * Return: multiplied char
  */
-int f5(char *usrn, int len)
+int multiply_chars(char *str, int len)
 {
-	int ch;
-	int vch;
+	int max_char, current_char;
 
-	ch = vch = 0;
+	max_char = current_char = 0;
 
-	while (vch < len)
+	while (current_char < len)
 	{
-		ch = ch + usrn[vch] * usrn[vch];
-		vch += 1;
+		max_char = max_char + str[current_char] * str[current_char];
+		current_char += 1;
 	}
 
-	return (((unsigned int)ch ^ 239) & 63);
+	return (((unsigned int)max_char ^ 239) & RAND_MOD);
 }
 
 /**
- * f6 - generates a random char
+ * generate_rand_char - generates a random char
+ * @str: username
  *
- * @usrn: username
  * Return: a random char
  */
-int f6(char *usrn)
+int generate_rand_char(char *str)
 {
-	int ch;
-	int vch;
+	int max_char, current_char;
 
-	ch = vch = 0;
+	max_char = current_char = 0;
 
-	while (vch < *usrn)
+	while (current_char < *str)
 	{
-		ch = rand();
-		vch += 1;
+		max_char = rand();
+		current_char += 1;
 	}
 
-	return (((unsigned int)ch ^ 229) & 63);
+	return (((unsigned int)max_char ^ 229) & RAND_MOD);
 }
 
 /**
  * main - Entry point
- *
  * @argc: arguments count
  * @argv: arguments vector
+ *
  * Return: Always 0
  */
 int main(int argc, char **argv)
 {
-	char keygen[7];
-	int len, ch, vch;
+	(void) argc;
+	char keygen[MAX_KEY_LEN];
+	int len, max_char, current_char;
 	long alph[] = {
 		0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
 		0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
 		0x723161513346655a, 0x6b756f494b646850 };
-	(void) argc;
 
 	for (len = 0; argv[1][len]; len++)
 		;
-	keygen[0] = ((char *)alph)[(len ^ 59) & 63];
-	ch = vch = 0;
-	while (vch < len)
+	keygen[0] = ((char *)alph)[(len ^ 59) & RAND_MOD];
+	max_char = current_char = 0;
+	while (current_char < len)
 	{
-		ch = ch + argv[1][vch];
-		vch = vch + 1;
+		max_char = max_char + argv[1][current_char];
+		current_char = current_char + 1;
 	}
-	keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
-	ch = 1;
-	vch = 0;
-	while (vch < len)
+	keygen[1] = ((char *)alph)[(max_char ^ 79) & RAND_MOD];
+	max_char = 1;
+	current_char = 0;
+	while (current_char < len)
 	{
-		ch = argv[1][vch] * ch;
-		vch = vch + 1;
+		max_char = argv[1][current_char] * max_char;
+		current_char = current_char + 1;
 	}
-	keygen[3] = ((char *)alph)[f4(argv[1], len)];
-	keygen[4] = ((char *)alph)[f5(argv[1], len)];
-	keygen[5] = ((char *)alph)[f6(argv[1])];
+	keygen[3] = ((char *)alph)[get_max_char(argv[1], len)];
+	keygen[4] = ((char *)alph)[multiply_chars(argv[1], len)];
+	keygen[5] = ((char *)alph)[generate_rand_char(argv[1])];
 	keygen[6] = '\0';
-	for (ch = 0; keygen[ch]; ch++)
-		printf("%c", keygen[ch]);
+	for (max_char = 0; keygen[max_char]; max_char++)
+		printf("%c", keygen[max_char]);
+
 	return (0);
 }
