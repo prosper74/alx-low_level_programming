63ef7851d802f0b275c4561ea845097af6851bdc
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-02-02 15:12:28 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,41 +1,53 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_set - Adds an element to the hash table.
-* @ht: The hash table.
-* @key: The key string.
-* @value: The value associated with the key.
-*
-* Return: 1 if successful, 0 otherwise.
-*/
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-unsigned long int index;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-if (ht == NULL || key == NULL || *key == '\0')
-	return (0);
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
 
-index = key_index((const unsigned char *)key, ht->size);
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
 
-hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
 
-if (new_node == NULL)
-	return (0);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
-new_node->key = strdup(key);
-new_node->value = strdup(value);
-
-if (new_node->key == NULL || new_node->value == NULL)
-{
-	free(new_node->key);
-	free(new_node->value);
-	free(new_node);
-	return (0);
+	return (1);
 }
-
-new_node->next = ht->array[index];
-ht->array[index] = new_node;
-
-return (1);
-}
-
