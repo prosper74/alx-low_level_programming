b9621e817401f23464ac354168bfabca8d0ad30d
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-02-04 15:48:54 +0100

    Update 100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -140,52 +140,6 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
 }
 
 /**
- * shash_table_print - Prints a sorted hash table in order.
- * @ht: A pointer to the sorted hash table.
- */
-void shash_table_print(const shash_table_t *ht)
-{
-  shash_node_t *node;
-
-  if (ht == NULL)
-    return;
-
-  node = ht->shead;
-  printf("{");
-  while (node != NULL)
-  {
-    printf("'%s': '%s'", node->key, node->value);
-    node = node->snext;
-    if (node != NULL)
-      printf(", ");
-  }
-  printf("}\n");
-}
-
-/**
- * shash_table_print_rev - Prints a sorted hash table in reverse order.
- * @ht: A pointer to the sorted hash table to print.
- */
-void shash_table_print_rev(const shash_table_t *ht)
-{
-  shash_node_t *node;
-
-  if (ht == NULL)
-    return;
-
-  node = ht->stail;
-  printf("{");
-  while (node != NULL)
-  {
-    printf("'%s': '%s'", node->key, node->value);
-    node = node->sprev;
-    if (node != NULL)
-      printf(", ");
-  }
-  printf("}\n");
-}
-
-/**
  * shash_table_delete - Deletes a sorted hash table.
  * @ht: A pointer to the sorted hash table.
  */
